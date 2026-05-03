abaa06fd4eb3c9842ee6d774fa046b7c256b4f02
Raju <rajubot793@gmail.com>
2026-05-03 19:33:41 +0600

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: rajubot,
+  author: '‎',
+  botName: "raju Bot",
+  botOwner: '𓄂𝐑𝐀𝐉𝐔 𝟒𝐗', // Your name
+  ownerNumber: '8801629143977', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "4.0.0",
+  updateZipUrl: "https://github.com/rajubot793-art",
+};
+
+module.exports = settings;
